dea82e3b84fc73b7a6384e4de7a21d1dd286e9af
Laurentiu Molnar <lau.molnar2013@gmail.com>
2018-05-28 19:59:59 +0300

    Updates

--- index.js
+++ index.js
@@ -3,16 +3,38 @@ const app = express();
 
 const rpio = require('rpio');
 
+const passport = require('passport');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const bodyParser = require('body-parser');
+
 const pinout = ["", "3v3", "5v", "gpio2", "5v", "gpio3","gnd","gpio4","gpio14","gnd","gpio15","gpio17", "gpio18","gpio27","gnd","gpio22","gpio23","3v3","gpio24","gpio10","gnd","gpio9","gpio25","gpio11","gpio8","gnd","gpio7","gpio0","gpio1","gpio5","gnd", "gpio6","gpio12","gpio13","gnd","gpio19","gpio16","gpio26","gpio20","gnd","gpio21"]
   .map(el => el.toUpperCase());
 
   // console.log(pinout.length);
 
 app.use(require("nocache")());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+require('./authentication').init(app);
+
+app.use(session({
+  store: new RedisStore({
+    url: "redis://localhost:8000"
+  }),
+  secret: 'my-super-secret',
+  resave: false,
+  saveUninitalized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'pug');
 
-app.get("/", (req, res) => res.render("index", {title: "Remote GPIO control", pinout: pinout}));
+app.get("/", passport.authenticationMiddleware, (req, res) => res.render("index", {title: "Remote GPIO control", pinout: pinout}));
 
 app.get("/gpio", (req, res) => {
   console.log(req.query);
@@ -43,6 +65,9 @@ app.get("/gpio", (req, res) => {
 
 });
 
+app.get('/auth', (req, res) => {
+  res.send("Auth page");
+});
 
 app.use("/static", express.static("static"));
 
