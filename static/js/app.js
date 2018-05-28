e4886997b06b533756bacfe04ef569d973997076
Laurentiu Molnar <lau.molnar2013@gmail.com>
2018-05-28 12:33:10 +0300

    Updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('input[type="submit"]')
-  .forEach((el) => el.addEventListener('submit', (evt) => {
+  .forEach((el) => el.addEventListener('click', (evt) => {
     evt.preventDefault();
 
     console.log(evt.target.id);
